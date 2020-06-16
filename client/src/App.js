682114a2d8927c50f5315fe50cee0cfc0a69928f
lyntechi <lynda-santiago@lambdastudents.com>
2020-06-16 01:43:02 -0400

    added second route for <Movie /> component

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MovieList from "./Movies/MovieList";
 import SavedList from "./Movies/SavedList";
-import Movie from "./Movies/Movie"
-import { Route } from "react-router-dom";
+import Movie from "./Movies/Movie";
+import MovieCard from './Movies/MovieCard'
+import { Route, Link, Switch, NavLink } from "react-router-dom";
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
@@ -30,30 +31,26 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-     
-        <div>
-        <Route exact path="/">
+
+      <div>
+        <Switch>
+          <Route path="/movies/:id">
+            <Movie movies={movieList} />
+          </Route>
+          <Route path="/">
             <MovieList movies={movieList} />
           </Route>
-
-        <Route path="/movies/:id">
-          <Movie />
-        </Route>
-       
-          
-        </div>
-   
+        </Switch>
+      </div>
     </div>
   );
 };
 
 export default App;
 
-
-
 // * [ ] Make it so that the card in `MovieList`
-//  is a link, this should direct the user to the 
-//  `/movies/{id of movie here}` URL, where `:id` is 
+//  is a link, this should direct the user to the
+//  `/movies/{id of movie here}` URL, where `:id` is
 //  the id of the individual movie.
 // * [ ] When a user clicks on a movie card they
 //  should be taken to `/movies/{id of movie here}`
@@ -61,7 +58,7 @@ export default App;
 // * [ ] You will need to modify line 13 of `Movie.js`
 //  in order to accept the correct id for the movie
 //   selected.
-// * [ ] Add functionality so the `Home` button on the 
+// * [ ] Add functionality so the `Home` button on the
 // `SavedList` component navigates back to home.
 // * [ ] You should now be able to navigate back and
 //  forth between the individual movies and the home
